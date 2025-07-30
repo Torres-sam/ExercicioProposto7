5f1a9fcf9dd74ce050196731a71192161b7e4bb4
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-30 16:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioProposto7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("O número é negativo !");
+            }
+            else if (n == 0) {
+                Console.WriteLine("O número é 0");
+            }
+            else
+            {
+                Console.WriteLine("O número é positivo !");
+            }
+        }
+    }
+}
